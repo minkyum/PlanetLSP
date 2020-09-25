ab1e3d8a316ad595f0114f5d612e047321901229
minkyum <moon.minkyu@gmail.com>
2020-09-25 09:35:18 -0400

    run for phenometrics

--- run_sh_script.R
+++ run_sh_script.R
@@ -1,5 +1,8 @@
-setwd('/projectnb/modislc/users/mkmoon/Planet/phe/')
-for(tt in 1:11){
-  system(paste('qsub -V -pe omp 8 -l h_rt=01:00:00 /usr3/graduate/mkmoon/GitHub/PlanetLSP/run_script.sh ',tt,sep=''))  
+vgt <- c('DB','MF','EN','AG','GR','SH')
+for(ss in 1:6){
+  setwd(paste0('/projectnb/modislc/users/mkmoon/Planet/phe/',vgt[ss]))
+  for(cc in 1:20){
+    system(paste('qsub -V -pe omp 8 -l h_rt=04:00:00 /usr3/graduate/mkmoon/GitHub/PlanetLSP/run_script.sh ',ss,cc,sep=''))  
+  }
 }
 
